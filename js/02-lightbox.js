e8ed47da24764f87aa6f82f8f6d53a7731860c0d
Karyna-Borysenko <k.a.borysenko@gmail.com>
2022-08-12 12:19:59 +0300

    02-lightbox-done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const imagesMarkup = createGallery(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", imagesMarkup);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
+    })
+    .join("");
+}
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: `alt`,
+  captionDelay: 250,
+});
