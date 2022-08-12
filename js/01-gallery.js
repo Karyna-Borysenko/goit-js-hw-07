fd04533e10c9cc376740c0d5651e55041e971914
Karyna-Borysenko <k.a.borysenko@gmail.com>
2022-08-12 11:24:18 +0300

    01-gallery-done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -10,7 +10,7 @@ function createGallery(galleryItems) {
     .map(({ preview, original, description }) => {
       return `
         <div class="gallery__item">
-            <a class="gallery__link" href="large-image.jpg">
+            <a class="gallery__link" href="${original}">
                 <img
                 class="gallery__image"
                 src="${preview}"
